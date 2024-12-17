37108fcd58eb1032b61de781761ed6e8c344fb8e
salonijuneja93 <salonijuneja93@gmail.com>
2024-12-17 15:22:41 -0800

    Add files via upload

---
+++ BookLibraryApp/backend/models/Book.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+const BookSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  status: { type: String, enum: ['Read', 'Unread'], default: 'Unread' },
+});
+module.exports = mongoose.model('Book', BookSchema);
